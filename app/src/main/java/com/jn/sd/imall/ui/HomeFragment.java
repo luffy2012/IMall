4337907636f1ed3384a04d714108f04731c107a5
alenX <genhaoai@gmail.com>
2015-08-18 11:18:51 +0800

    广告栏定时器调整

--- app/src/main/java/com/jn/sd/imall/ui/HomeFragment.java
+++ app/src/main/java/com/jn/sd/imall/ui/HomeFragment.java
@@ -27,28 +27,16 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 
-public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeListener{
-    private static final int PAGE_SIZE = 3;//主页广告切换栏总的广告数目
+public class HomeFragment extends Fragment implements ViewPager.OnPageChangeListener {
     private ViewPager mViewPager;//广告栏ViewPager
-    private PagerAdapter mPagerAapter;//ViewPager的内容适配器
-    private List<View> mList;//页面信息数组
 
-
-    private ImageView[] tips;
+    private ImageView[] tips;//圆点图标图片数组
     private ImageView[] mImageViews;//ViewPaper广告图片
-    private int[] imgIdArray ;
-
-
-    private View mFirstView,mSencondView,mThirdView;
+    private int[] imgIdArray;//ViewPager广告图片资源ID
 
     private ScheduledExecutorService mScheduledSer;//定时器，定时处理广告的切换操作
-    private int currentItem=(3) * 100;//当前
+    private int currentItem = (3) * 100;//当前
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //TODO 初始化 mViewPager
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,21 +44,6 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
 
         View contactsLayout = inflater.inflate(R.layout.fragment_home,
                 container, false);
-
-        /*mFirstView = inflater.inflate(R.layout.home_ads_page_view_first,
-                container, false);
-        mSencondView = inflater.inflate(R.layout.home_ads_page_view_second,
-                container, false);
-        mThirdView = inflater.inflate(R.layout.home_ads_page_view_third,
-                container, false);
-        mList = new ArrayList<>(PAGE_SIZE);
-        mList.add(mFirstView);
-        mList.add(mSencondView);
-        mList.add(mThirdView);
-        mViewPager = (ViewPager)contactsLayout.findViewById(R.id.home_mall_ads_view_pager);
-        mViewPager.setAdapter(new AdsViewPagerAdapter(mList));*/
-
-
         /***  动态指定ViewPaper的大小 start by luffy  **/
         DisplayMetrics dm = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -78,25 +51,25 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
         Bitmap bm = BitmapFactory.decodeResource(res, R.drawable.bg_test);
         int tempWidth = bm.getWidth();
         int tempHeight = bm.getHeight();
-        RelativeLayout layout =  (RelativeLayout)contactsLayout.findViewById(R.id.viewpaper_layout);
+        RelativeLayout layout = (RelativeLayout) contactsLayout.findViewById(R.id.viewpaper_layout);
         LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) layout.getLayoutParams();
         params.height = (int) (tempHeight * ((double) dm.widthPixels / (double) tempWidth));
         layout.setLayoutParams(params);
         /***  动态指定ViewPaper的大小 end  by luffy  **/
 
 
-        ViewGroup group = (ViewGroup)contactsLayout.findViewById(R.id.viewGroup);
+        ViewGroup group = (ViewGroup) contactsLayout.findViewById(R.id.viewGroup);
         mViewPager = (ViewPager) contactsLayout.findViewById(R.id.home_mall_ads_view_pager);
 
         imgIdArray = new int[]{R.drawable.bg_test, R.drawable.bg_test, R.drawable.bg_test};
         tips = new ImageView[imgIdArray.length];
-        for(int i=0; i<tips.length; i++){
+        for (int i = 0; i < tips.length; i++) {
             ImageView imageView = new ImageView(getActivity().getApplicationContext());
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(10,10));
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(10, 10));
             tips[i] = imageView;
-            if(i == 0){
+            if (i == 0) {
                 tips[i].setBackgroundResource(R.drawable.page_indicator_focused);
-            }else{
+            } else {
                 tips[i].setBackgroundResource(R.drawable.page_indicator_unfocused);
             }
 
@@ -110,7 +83,7 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
 
         //初始化广告的图片
         mImageViews = new ImageView[imgIdArray.length];
-        for(int i=0; i<mImageViews.length; i++){
+        for (int i = 0; i < mImageViews.length; i++) {
             ImageView imageView = new ImageView(getActivity().getApplicationContext());
             mImageViews[i] = imageView;
             imageView.setBackgroundResource(imgIdArray[i]);
@@ -122,8 +95,12 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
         //设置ViewPager的默认项, 设置为长度的100倍，这样子开始就能往左滑动
         mViewPager.setCurrentItem((mImageViews.length) * 100);
 
+        startTimerTask();
+        return contactsLayout;
+    }
 
-//        mScheduledSer = Executors.newSingleThreadScheduledExecutor();
+    private void startTimerTask() {
+        //        mScheduledSer = Executors.newSingleThreadScheduledExecutor();
         //通过定时器 来完成 每2秒钟切换一个图片
         //经过指定的时间后，执行所指定的任务
         //scheduleAtFixedRate(command, initialDelay, period, unit)
@@ -132,34 +109,6 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
         //period  每间隔多次时间来重新启动任务
         //unit 时间单位
 //        mScheduledSer.scheduleAtFixedRate(new ViewPagerTask(), 1, 1, TimeUnit.SECONDS);
-
-
-
-
-        return contactsLayout;
-
-        /** add by wangss
-        View contactsLayout = inflater.inflate(R.layout.home_tools_bar,
-                container, false);
-
-        mFirstView = inflater.inflate(R.layout.home_ads_page_view_first,
-                container, false);
-        mSencondView = inflater.inflate(R.layout.home_ads_page_view_second,
-                container, false);
-        mThirdView = inflater.inflate(R.layout.home_ads_page_view_third,
-                container, false);
-        mList = new ArrayList<>(PAGE_SIZE);
-        mList.add(mFirstView);
-        mList.add(mSencondView);
-        mList.add(mThirdView);
-        mViewPager = (ViewPager)contactsLayout.findViewById(R.id.home_mall_ads_view_pager);
-        mViewPager.setAdapter(new AdsViewPagerAdapter(mList));
-
-        return contactsLayout;
-         */
-    }
-
-    private void initViews(View view) {
 
     }
 
@@ -178,47 +127,18 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
     public void onPageScrollStateChanged(int state) {
 
     }
+
     private void setImageBackground(int selectItems) {
-        for(int i=0; i<tips.length; i++){
-            if(i == selectItems){
+        for (int i = 0; i < tips.length; i++) {
+            if (i == selectItems) {
                 tips[i].setBackgroundResource(R.drawable.page_indicator_focused);
-            }else{
+            } else {
                 tips[i].setBackgroundResource(R.drawable.page_indicator_unfocused);
             }
         }
     }
 
-    /*private class AdsViewPagerAdapter extends PagerAdapter{
-
-        private List<View> mListViews;
-
-        public AdsViewPagerAdapter(List<View> mListViews) {
-            this.mListViews = mListViews;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(mListViews.get(position));
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            container.addView(mListViews.get(position),0);
-            return mListViews.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mListViews.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view==object;
-        }
-    }*/
-
-    public class AdsViewPagerAdapter extends PagerAdapter{
+    public class AdsViewPagerAdapter extends PagerAdapter {
 
         @Override
         public int getCount() {
@@ -242,9 +162,9 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
         @Override
         public Object instantiateItem(View container, int position) {
             try {
-                ((ViewPager)container).addView(mImageViews[position % mImageViews.length], 0);
+                ((ViewPager) container).addView(mImageViews[position % mImageViews.length], 0);
 
-            }catch (Exception e){
+            } catch (Exception e) {
 
             }
             return mImageViews[position % mImageViews.length];
@@ -252,38 +172,34 @@ public class HomeFragment extends Fragment  implements ViewPager.OnPageChangeLis
 
 
     }
-    private class ViewPagerTask implements Runnable{
-         Handler handler = new Handler(){
+
+    private class ViewPagerTask implements Runnable {
+        Handler handler = new Handler() {
 
             @Override
             public void handleMessage(Message msg) {
                 //设定viewPager当前页面
-                switch (msg.what){
+                switch (msg.what) {
                     case 1:
                         mViewPager.setCurrentItem(currentItem);
-                        Log.i("bb",currentItem+"");
+                        Log.i("bb", currentItem + "");
                         break;
 
                 }
 
             }
         };
+
         public void run() {
             //实现我们的操作
             //改变当前页面
-            Log.i("aa",currentItem+"");
+            Log.i("aa", currentItem + "");
             currentItem = (currentItem + 1) % mImageViews.length;
-            Log.i("cc",currentItem+"");
+            Log.i("cc", currentItem + "");
             //Handler来实现图片切换
             Message message = new Message();
-            message.what=1;
+            message.what = 1;
             handler.sendMessage(message);
         }
     }
-
-
-
-
-
 }
-
